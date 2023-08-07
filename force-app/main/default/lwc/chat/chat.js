78ee2f32e40294bb00d83bc26ec002bc9c3b2682
Nico Linares Mayer <niliyer001@gmail.com>
2023-08-07 12:13:01 -0300

    chat working

--- force-app/main/default/lwc/chat/chat.js
+++ force-app/main/default/lwc/chat/chat.js
@@ -5,7 +5,7 @@ import retrieveChatMessages from '@salesforce/apex/ChatController.retrieveChatMe
 export default class Chat extends LightningElement {
     @api recordId;
     prompt = '';
-    response = 'no response';
+    response = '';
     messages = [];
     handleInputChange(event) {
         this.prompt = event.target.value;
@@ -13,34 +13,49 @@ export default class Chat extends LightningElement {
 
     @wire(retrieveChatMessages,{sessionId : '$recordId'})
     wiredRetrieveChatMessages({error,data}){
-        if (data) {
-            let index = 1;
-            this.messages = data.map(element=>{
-                let object = JSON.parse(element);
-                object.Id = index;
-                index++;
-                return object;
-            });
-            console.log(JSON.parse(JSON.stringify(this.messages)));
-        } else if (error) {
-            console.error(error);
+        try{
+            if (data) {
+                let index = 1;
+    
+                this.messages = data.map(element=>{
+                    let object = JSON.parse(element);
+                    object['Id'] = index;
+                    index++;
+                    return object;
+                });
+                console.log(JSON.parse(JSON.stringify(this.messages)));
+            } else if (error) {
+                console.error(error);
+                console.log('error-->'+error);
+            }
         }
+        catch(error){
+            console.log(error);
+        }
+        
     }
 
     handleButtonClick(){
         this.response = 'Waiting for response...';
+        console.log(this.prompt);
         sendPrompt({ prompt: this.prompt ,sessionId : this.recordId})
         .then(result => {
             // Aquí puedes manejar el resultado del método de Apex
             console.log('Resultado de Apex:', result);
-            let newMessages = [{role:"user",content:this.prompt, Id : 123},{role: "system" , content: result, Id : 1234}];
-            this.response = result;
-            this.messages = [...this.messages, newMessages[0],newMessages[1]];
-            this.prompt = '';
+            if(result!= ''){
+                let newMessages = [{role:"user",content:this.prompt, Id : 123},{role: "system" , content: result, Id : 1234}];
+                this.response = '';
+                this.messages = [...this.messages, newMessages[0],newMessages[1]];
+                this.prompt = '';
+            }
+            else{
+                this.response = 'Error'
+            }
+            
             
         })
         .catch(error => {
-            // Manejar errores si es necesario
+            this.response = 'Error'
             console.error(error);
         });
     }  
